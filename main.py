fa88f67f900f5593b91b4f76c5ea6eeae168b829
Werikly <mi20090803@gmail.com>
2024-04-27 15:00:33 +0400

    added turtle

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import turtle
 import keyboard
+
+cherepaha = turtle.Turtle()
+
+while True:
+    if keyboard.is_pressed("w"):
+        cherepaha.forward(10)
+    elif keyboard.is_pressed("a"):
+        cherepaha.left(5)
+    elif keyboard.is_pressed("d"):
+        cherepaha.right(5)
+    elif keyboard.is_pressed("s"):
+        cherepaha.back(10)
+    elif keyboard.is_pressed("esc"):
+        break
+    
+
+cherepaha.done
